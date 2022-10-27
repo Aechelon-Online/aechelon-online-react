2bd7933095d6896ecd715973a652e32ebdecaf75
Aechelon-Online <info@aechelon.online>
2022-10-26 19:57:33 -0500

    Added transitions for Techstack links

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 export default function Card(props) {
@@ -8,18 +7,23 @@ export default function Card(props) {
             <Scaled className="techstack-page__flexbox-text">
                 <a target="_blank" href={props.url}>
                     {props.title}
-                </a>
+                </a> 
             </Scaled>
+           
         </div>
     )
 }
 
 const Scaled = styled.h3`
     
+    position: relative;
     font-size: clamp(1.5rem, 10vw, 4rem);
+    display: flex;
+    justify-content: center;
     margin: 2vh 0;
     font-family: Lato, sans-serif;
-
+    transform: scale(1);
+    transition: 200ms;
     & a {
         color: white;
     }
@@ -34,4 +38,26 @@ const Scaled = styled.h3`
 		0 0 20px rgba(255, 255, 255, 1),
 		0 0 30px rgba(255, 255, 255, 1);
     }
+`
+const DemoButton = styled.button`
+    
+    border-radius: 10px;
+    margin-right: 5px;
+    width: clamp(50px, 5vw, 100px);
+    font-size: clamp(0.2rem, 2vw, 1rem);
+`
+
+const CodeButton = styled.button`
+    
+    border-radius: 10px;
+    margin-left: 5px;
+    width: clamp(50px, 5vw, 100px);
+    font-size: clamp(0.2rem, 2vw, 1rem);
+`
+
+const ButtonDiv = styled.div`
+    
+    position: absolute;
+    top: 40%;
+    display: flex;
 `
